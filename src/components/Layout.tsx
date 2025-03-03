4141956a7fd83b6c9b4cfe368c814306a83a81a5
WuTangLan1 <finnmassari16@gmail.com>
2025-03-03 11:43:03 +0200

    + Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+// src/components/Layout.tsx
+import { ReactNode, useRef } from "react";
+import Header from "./Header";
+interface LayoutProps {
+  children: (refs: { aboutRef: React.RefObject<HTMLElement | null>; clipsRef: React.RefObject<HTMLElement | null>; eventsRef: React.RefObject<HTMLElement | null>; }) => ReactNode;
+}
+const Layout = ({ children }: LayoutProps) => {
+  const aboutRef = useRef<HTMLElement | null>(null);
+  const clipsRef = useRef<HTMLElement | null>(null);
+  const eventsRef = useRef<HTMLElement | null>(null);
+  return (
+    <div className="scroll-smooth">
+      <Header aboutRef={aboutRef} clipsRef={clipsRef} eventsRef={eventsRef} />
+      <main className="pt-24">
+        {children({ aboutRef, clipsRef, eventsRef })}
+      </main>
+    </div>
+  );
+};
+export default Layout;
